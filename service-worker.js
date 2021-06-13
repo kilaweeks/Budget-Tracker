13afa5d64a16472361bc562f87afd2d3ca891718
kiweeks <kilaweeks@gmail.com>
2021-06-12 21:22:48 -0700

    added db.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    `/`,
+    `/index.html`,
+    `/db.js`,
+    `/index.js`,
+    `/manifest.webmanifest`,
+    `/assets/icons/moneyicon512x512.png`,
+    `/assets/icons/moneyicon192x192.png`,
+    `/styles.css`,
+  ];
+
